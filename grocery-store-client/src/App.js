ccb453669cf8088c4877f417ec5a2713a4e3af2b
michalReiner <michal3213031@gmail.com>
2025-04-08 21:42:07 +0300

    remove comment

--- grocery-store-client/src/App.js
+++ grocery-store-client/src/App.js
@@ -1,26 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Register from './pages/Register';
-// import Login from './pages/Login';
-// import SupplierOrders from './pages/SupplierOrders';
-// import CreateOrder from './pages/CreateOrder';
-// import Navbar from './components/Navbar'; // ייבוא הניווט החדש
-
-// function App() {
-//   return (
-//     <Router>
-//       <Navbar /> {/* הוספת הניווט למעלה */}
-//       <Routes>
-//         <Route path="/register" element={<Register />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/orders" element={<SupplierOrders />} />
-//         <Route path="/create-order" element={<CreateOrder />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/Login';
